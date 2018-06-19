c58c0be35fc488e3721dfedf86fc13f66cad4be9
carlosblade75 <carlosblade75@hotmail.com>
2018-06-19 13:51:33 +0200

    Variable entorno

--- server/config/config.js
+++ server/config/config.js
@@ -17,10 +17,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:abc123@ds261660.mlab.com:61660/moongodb_cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
